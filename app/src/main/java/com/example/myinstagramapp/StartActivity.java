abd56e54afc19196ed70944fc56aae16319fa8b7
PanicLuka <lukap181@gmail.com>
2021-08-16 13:24:25 +0200

    novi commit

--- app/src/main/java/com/example/myinstagramapp/StartActivity.java
+++ app/src/main/java/com/example/myinstagramapp/StartActivity.java
@@ -80,14 +80,14 @@ public class StartActivity extends AppCompatActivity {
         }
     }
 
-//    @Override
-//    protected void onStart()
-//    {
-//        super.onStart();
-//        if (FirebaseAuth.getInstance().getCurrentUser() != null){
-//            startActivity(new Intent(StartActivity.this, MainActivity.class));
-//            finish();
-//        }
-//    }
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        if (FirebaseAuth.getInstance().getCurrentUser() != null){
+            startActivity(new Intent(StartActivity.this, MainActivity.class));
+            finish();
+        }
+    }
 
 }
